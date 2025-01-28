5ded23d90c2af094a4dc815fdf7d1d29515ea2d4
Pedro Cavaleiro <p.cavaleiro@icloud.com>
2025-01-28 18:22:56 +0000

    Re Adds previously removed button

--- src/UserInput.vue
+++ src/UserInput.vue
@@ -31,6 +31,21 @@
           }}
         </span>
       </div>
+      <span
+        v-tooltip.bottom="t('chat.deleteFile')"
+        class="delete-file-message"
+        @click="cancelFile()"
+      >
+        <svg
+          viewBox="0 0 47.971 47.971"
+          xmlns="http://www.w3.org/2000/svg"
+          :style="`width: 15px; height: 15px; fill: ${colors.userInput.text}`"
+        >
+          <path
+            d="M28.228,23.986L47.092,5.122c1.172-1.171,1.172-3.071,0-4.242c-1.172-1.172-3.07-1.172-4.242,0L23.986,19.744L5.121,0.88   c-1.172-1.172-3.07-1.172-4.242,0c-1.172,1.171-1.172,3.071,0,4.242l18.865,18.864L0.879,42.85c-1.172,1.171-1.172,3.071,0,4.242   C1.465,47.677,2.233,47.97,3,47.97s1.535-0.293,2.121-0.879l18.865-18.864L42.85,47.091c0.586,0.586,1.354,0.879,2.121,0.879   s1.535-0.293,2.121-0.879c1.172-1.171,1.172-3.071,0-4.242L28.228,23.986z"
+          />
+        </svg>
+      </span>
     </div>
     <form
       class="sc-user-input"
@@ -64,7 +79,7 @@
           <FileIcons
             :on-change="_handleFileSubmit"
             :color="colors.userInput.text"
-            :acceptedFileTypes="acceptedFileTypes"
+            :accepted-file-types="acceptedFileTypes"
           />
         </div>
         <div v-if="isEditing" class="sc-user-input--button">
